ffbe6e4cb06d342b66388822e89b42a92f93854b
daw2-munoz22 <91275885+daw2-munoz22@users.noreply.github.com>
2023-06-07 11:10:07 +0200

    Delete src directory

--- src/bd/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://efktoowuedoipllrdoyq.supabase.co'
-
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVma3Rvb3d1ZWRvaXBsbHJkb3lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU0Mjk0NzIsImV4cCI6MjAwMTAwNTQ3Mn0.w9DFFtfi8TZojfMJ82bcwVTybbpiNtafypXGAsXLg3k'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
-
-console.log('Conexion a supabase', supabase)
